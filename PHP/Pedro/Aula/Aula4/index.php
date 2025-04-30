9b01037e3794f55c90e48cd3e2229de99d6e6635
PedroStonks <pedrohenriquemigue76@gmail.com>
2025-04-30 11:24:09 -0300

    .

--- PHP/Pedro/Aula/Aula4/index.php
+++ PHP/Pedro/Aula/Aula4/index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+<form action="soma.php" method="GET">
+        <label for="valorA">ValorA:</label>     
+        <input name="valorA" type="number" placeholder="ex: 1"></input>
+
+        <label for="valorB">valorB:</label>     
+        <input name="valorB" type="number" placeholder="ex: 2"></input>
+
+        <label for="valorC">valorC:</label> 
+        <input name="valorC" type="number" placeholder="ex: 3"></input>
+
+        <label for="valorD">valorD:</label> 
+        <input name="valorD" type="number" placeholder="ex: 4"></input>
+
+
+        <button tipe="submit">Calcular</button>
+    </form>
     
 </body>
 </html>
